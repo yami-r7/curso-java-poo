b3a15b2676ba5149bfe5c7c9fef9f6ce813406f5
yami-r7 <rafaelyami7@hotmail.com>
2025-09-01 21:10:18 -0300

    Visibilidade de atributos e métodos

---
+++ src/aula02/visibilidadeatributosmetodos/Caneta.java
@@ -0,0 +1,33 @@
+package aula02.visibilidadeatributosmetodos;
+
+public class Caneta {
+    public String modelo;
+    public String cor;
+    private float ponta;
+    protected int carga;
+    protected boolean tampada;
+
+    public void status() {
+        System.out.println("Modelo: " + this.modelo);
+        System.out.println("Cor: " + this.cor);
+        System.out.println("Ponta: " + this.ponta);
+        System.out.println("Carga: " + this.carga);
+        System.out.println("Está tampada? " + this.tampada);
+    }
+
+    public void rabiscar() {
+        if(this.tampada == true) {
+            System.out.println("ERRO! Não posso rabiscar.");
+        }else {
+            System.out.println("Rabiscando...");
+        }
+    }
+
+    private void tampar() {
+        this.tampada = true;
+    }
+
+    private void destampar() {
+        this.tampada = false;
+    }
+}
