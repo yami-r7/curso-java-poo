4137a4fdf16a967e255177f8c0bc7f3c1b847f02
yami-r7 <rafaelyami7@hotmail.com>
2025-09-02 22:20:15 -0300

    Métodos Getter, Setter e Constructor

--- src/Aula03/gettersetterconstructor/Caneta.java
+++ src/Aula03/gettersetterconstructor/Caneta.java
@@ -3,26 +3,53 @@ package Aula03.gettersetterconstructor;
 public class Caneta {
     public String modelo;
     private float ponta;
+    private String cor;
+    private boolean tampada;
 
-    public void status() {
-        System.out.println("Sobre a caneta:");
-        System.out.println("Modelo: " + this.modelo);
-        System.out.println("Ponta: " + this.ponta);
+    public Caneta(String modelo, float ponta, String cor) {
+        this.modelo = modelo;
+        this.ponta = ponta;
+        this.cor = cor;
     }
 
     public String getModelo() {
-        return this.modelo;
+        return modelo;
     }
 
-    public void setModelo(String m) {
-        this.modelo = m;
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
     }
 
     public float getPonta() {
-        return this.ponta;
+        return ponta;
     }
 
-    public void setPonta(float p) {
-        this.ponta = p;
+    public void setPonta(float ponta) {
+        this.ponta = ponta;
     }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public boolean isTampada() {
+        return tampada;
+    }
+
+    public void setTampada(boolean tampada) {
+        this.tampada = tampada;
+    }
+
+    public void status() {
+        System.out.println("Sobre a caneta:");
+        System.out.println("Modelo: " + this.getModelo());
+        System.out.println("Ponta: " + this.getPonta());
+        System.out.println("Cor: " + this.cor);
+        System.out.println("Tampada: " + this.tampada);
+    }
+
 }
--- src/Aula03/gettersetterconstructor/Main.java
+++ src/Aula03/gettersetterconstructor/Main.java
@@ -2,9 +2,9 @@ package Aula03.gettersetterconstructor;
 
 public class Main {
     public static void main(String[] args) {
-        Caneta c1 = new Caneta();
-        c1.setModelo("Bic");
-        c1.setPonta(0.5f);
+        Caneta c1 = new Caneta("Bic", 0.7f, "Preta");
         c1.status();
+        Caneta c2 = new Caneta("Compactor", 0.5f, "Vermelha");
+        c2.status();
     }
 }
